d5a2e6e9883db30076c5f6ed9a23639b045c6eb6
Moola <Moola.com>
2019-08-23 16:40:58 -0400

    Next verion of First Web File

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 (function(){
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+  angular.module('lunchCheck', [])
+  .controller('lunchCheckController', lunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
+  lunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController($scope) {
+  function lunchCheckController($scope) {
 
-$scope.lunchItem = "";
-$scope.message = "";
-$scope.messageClass = "";
+    $scope.lunchItem = "";
+    $scope.message = "";
+    $scope.messageClass = "";
 
-$scope.checkLunch = function() {
-  var lunchItemsArray = $scope.lunchItem.split(',');
-  var cleanArray = lunchItemsArray.filter(function(item){return (item && (+item)!=0 );}) // cleaning empty values
-  var lunchItemSum = cleanArray.length; // lenght = 1 - it's maybe empty array!!! Becouse it is not lenght, it's last index+1
+    $scope.checkLunch = function() {
+      // Splitting the array
+      var lunchItemsArray = $scope.lunchItem.split(',');
+      // cleaning empty values in the array
+      var cleanArray = lunchItemsArray.filter(function(item){return (item && (+item)!=0 );})
+      // To calculate number of Items entered
+      var lunchItemSum = cleanArray.length;
 
-  console.log(lunchItemSum);
+      console.log(lunchItemSum);
 
-  if(lunchItemSum <= 1 && !lunchItemsArray[0]){
-    $scope.message = "Please enter data first";
-    $scope.messageClass = "text-danger";
-  }else if (lunchItemSum < 4) {
-    $scope.message = "Enjoy!";
-    $scope.messageClass = "text-success";
-  }else if (lunchItemSum > 3) {
-    $scope.message = "Too much!";
-    $scope.messageClass = "text-danger";
-  }
-}
-
-};
-
+      if(lunchItemSum <= 1 && !lunchItemsArray[0]){
+        $scope.message = "Please enter data first";
+        $scope.messageClass = "text-danger";
+      }else if (lunchItemSum < 4) {
+        $scope.message = "Enjoy!";
+        $scope.messageClass = "text-success";
+      }else if (lunchItemSum > 3) {
+        $scope.message = "Too much!";
+        $scope.messageClass = "text-danger";
+      }
+    }
+  };
 })();
